af956ebb0e6de959cfd07523c430e0bb9b028bea
zahrabashir98 <zahra_9877@yahoo.com>
2019-05-16 00:58:05 +0430

    Split data into test(20%) and train(80%)

---
+++ p2/SplitData/src/src.py
@@ -0,0 +1,37 @@
+import random
+
+def split_data(all_words_list,label, num):
+
+    train_len = (len(all_words_list) * 8)//10
+    test_len = (len(all_words_list)) - (train_len)
+    # print(train_len)
+    # print(test_len )
+
+    test_data  = random.sample(all_words_list,test_len)
+    for test in test_data:
+        all_words_list.remove(test)
+    train_data = all_words_list
+
+    train_string = ""
+    with open("../%s/train/label%s.txt"%(label, num), "w") as f1:
+        for tr in train_data:
+            f1.write('%s ' % tr)
+        f1.close()
+
+    with open("../%s/test/label%s.txt"%(label, num), "w") as f2:
+        for te in test_data:
+            f2.write('%s ' % te)
+        f2.close()
+
+if __name__ == "__main__":
+    with open("../../ProcessedData/ImamKhomeini/label1.txt") as d:
+        string1 = d.read()
+        all_words_list = string1.split(' ')
+        split_data(all_words_list,"ImamKhomeini", "1")
+        print(len(all_words_list))
+
+    with open("../../ProcessedData/MohammadRezaPahlavi/label2.txt") as d:
+        string2 = d.read()
+        all_words_list = string2.split(' ')
+        split_data(all_words_list,"MohammadRezaPahlavi", "2")
+        print(len(all_words_list))
