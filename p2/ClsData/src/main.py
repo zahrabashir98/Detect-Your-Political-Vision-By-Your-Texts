7111073db46a84b99df248403196e575a95efc65
zahrabashir98 <zahra_9877@yahoo.com>
2019-06-10 18:43:27 +0430

    Organize ClsData subfolders and files

--- p2/ClsData/src/main.py
+++ p2/ClsData/src/main.py
@@ -6,30 +6,29 @@ import random
 
 def calculate_total_len():
 
-    with open('../ProcessedData/ImamKhomeini/concat_all_1 (copy).txt') as outfile1:
+    with open('../../ProcessedData/ImamKhomeini/tagged_data_1.txt') as outfile1:
         len1 = len(outfile1.readlines())
-    with open('../ProcessedData/MohammadRezaPahlavi/concat_all_2 (copy).txt') as outfile2:
+    with open('../../ProcessedData/MohammadRezaPahlavi/tagged_data_2.txt') as outfile2:
         len2 = len(outfile2.readlines())
     return len1 + len2
 
 # only run for the first time
-# for line in fileinput.input(["../ProcessedData/ImamKhomeini/concat_all_1 (copy).txt"], inplace=True):
+# for line in fileinput.input(["../ProcessedData/ImamKhomeini/tagged_data_1.txt"], inplace=True):
 #     sys.stdout.write('Imam {l}'.format(l=line))
-with open('../ProcessedData/ImamKhomeini/concat_all_1 (copy).txt') as outfile1:
+with open('../../ProcessedData/ImamKhomeini/tagged_data_1.txt') as outfile1:
     string1 = outfile1.read()
 
 
 # only run for the first time
-# for line in fileinput.input(["../ProcessedData/MohammadRezaPahlavi/concat_all_2 (copy).txt"], inplace=True):
+# for line in fileinput.input(["../ProcessedData/MohammadRezaPahlavi/tagged_data_2.txt"], inplace=True):
 #     sys.stdout.write('Shah {l}'.format(l=line))
-with open('../ProcessedData/MohammadRezaPahlavi/concat_all_2 (copy).txt') as outfile2:
+with open('../../ProcessedData/MohammadRezaPahlavi/tagged_data_2.txt') as outfile2:
     string2 = outfile2.read()
 
-with open("concat_2_files.txt", "w") as file1:
+with open("../merged_file.txt", "w") as file1:
     file1.write(string1+"\n"+string2)
 
 total_len = calculate_total_len()
-print(total_len)
 train_len  = int(0.8 * total_len)
 test_len = total_len - train_len
 
@@ -43,28 +42,20 @@ for index in test_index:
     total_index.remove(index)
 train_index = total_index
 
-print(len(train_index))
-# print(train_index)
-print(len(test_index))
-# print(test_index)
-
 array = []
-with open('concat_2_files.txt') as fille:
+with open('../merged_file.txt') as fille:
     a = fille.read()
     b = a.split("\n")
     for data in b :
         array.append(data)
 array.pop()
-print(len(array))
+# print(len(array))
 
 # array now has each line of concat data
-with open("train.txt", "w")as train_file:
+with open("../train.txt", "w")as train_file:
     for train_ind in train_index:
         train_file.write(array[train_ind]+"\n")
 
-with open("test.txt", "w")as test_file:
+with open("../test.txt", "w")as test_file:
     for test_ind in test_index:
         test_file.write(array[test_ind]+"\n")
-
-
-
