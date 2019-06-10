d3d53e921f6ea5a64c39a421cc6d8d348ee19651
zahrabashir98 <zahra_9877@yahoo.com>
2019-06-10 20:40:57 +0430

    Train the Naive bayes model

--- p2/ClsModel/src/main.py
+++ p2/ClsModel/src/main.py
@@ -10,6 +10,7 @@ label_1_count = 0
 label_2_count = 0
 label_1_vocab = 0
 label_2_vocab = 0
+
 for data in string :
 
     if data != "":
@@ -44,13 +45,67 @@ for data in string :
 
 label_1_frequencies = {}
 label_2_frequencies = {}
-# print(dictionary_1[])
+
+flag = True
 V = label_1_vocab + label_2_vocab
 for word in dictionary_1:
-    label_1_frequencies[word] = dictionary_1[word] + 1 / label_1_count + V
+    label_1_frequencies[word] = (dictionary_1[word] + 1) / (label_1_count + V)
 
 for word in dictionary_2:
-    label_2_frequencies[word] = dictionary_2[word] + 1 / label_2_count + V
+    label_2_frequencies[word] = (dictionary_2[word] + 1) / (label_2_count + V)
 
-# print(label_1_frequencies)
-# print(dictionary_1)
+#UNK
+label_1_frequencies["UNK"] = 1 / (label_1_count + V)
+label_2_frequencies["UNK"] = 1/ (label_2_count + V)
+
+# Test part
+with open("test.txt") as test:
+    t = test.read()
+    test_list = t.split("\n")
+test.close()
+
+p_label_1 = {}
+p_label_2 = {}
+
+for data in test_list:
+    print(data)
+    if data != "":
+        print("here")
+         # Imam
+        if data[0] == label1[0]:
+            new = data[len(label1)+1:]
+            sentence = new.split(" ")
+            print(sentence)
+            # jomle khali daram
+            p_1 = 1
+            p_2 = 1
+            for word in sentence:
+                p_1 *= label_1_frequencies[word]
+            for word in sentence:
+                p_2 *= label_2_frequencies[word]
+
+            p_label_1[sentence] = p_1
+            p_label_2[sentence] = p_2
+            print(p_label_1)
+            print(p_label_2)
+            p_1 = 1
+            p_2 = 1
+
+        # Shah
+        elif data[0] == label2[0]:
+            p_1 = 1
+            p_2 = 1
+            new = data[len(label2)+1:]
+            sentence = new.split(" ")
+            # jomle khali daram
+            for word in sentence:
+                p_1 *= label_1_frequencies[word]
+            for word in sentence:
+                p_2 *= label_2_frequencies[word]
+
+            p_label_1[sentence] = p_1
+            p_label_2[sentence] = p_2
+            print(p_label_1)
+            print(p_label_2)
+            p_1 = 1
+            p_2 = 1
